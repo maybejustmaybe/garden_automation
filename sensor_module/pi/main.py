9755901e0dd2f0a73b6863b82bdc36210511056d
maybejustmaybe <maybejustmaybe@gmail.com>
2022-02-03 11:08:59 -0500

    refactor test

--- sensor_module/pi/main.py
+++ sensor_module/pi/main.py
@@ -1,11 +1,18 @@
-import serial
-import json
+from lib import pyboard
+
+FEATHER_DEVICE = "/dev/ttyUSB0"
 
 def main():
-    port = serial.Serial("/dev/ttyUSB0", baudrate=115200)
-    while True:
-        readings = port.readline().decode("utf-8")
-        print(readings)
+    feather = pyboard.Pyboard(FEATHER_DEVICE)
+    try:
+        print("Testing...")
+        feather.exec("print('test')")
+        print("Reading...")
+        feather.read_until(1, "\n", data_consumer=print)
+    finally:
+        feather.close()
+
+    print("done")
 
 if __name__ == "__main__":
     main()
