6f24d1816ead7dc66fa2f58f0851a5704213cf9a
maybejustmaybe <maybejustmaybe@gmail.com>
2022-02-06 10:18:38 -0500

    test

--- sensor_module/pi/main.py
+++ sensor_module/pi/main.py
@@ -1,68 +1,72 @@
-import sys
-import importlib
-import importlib.util
+import logging
 from lib import pyboard
 from pathlib import Path
+import multiprocessing as mp
 
 FEATHER_DEVICE = "/dev/ttyUSB0"
+FEATHER_BAUD_RATE = 115200
 
 FEATHER_DIR_PATH = Path(__file__).resolve().parents[1] / "feather"
 FEATHER_MAIN_PATH = FEATHER_DIR_PATH / "main.py"
 FEATHER_LIB_DIR_PATH = FEATHER_DIR_PATH / "lib"
 
-def load_esptool_module():
-    ESPTOOL_PATH = Path(sys.exec_prefix) / "bin" / "esptool.py"
+# TODO : remove
+# import sys
+# import importlib
+# import importlib.util
+# def load_esptool_module():
+#     ESPTOOL_PATH = Path(sys.exec_prefix) / "bin" / "esptool.py"
+# 
+#     esptool_spec = importlib.util.spec_from_file_location("esptool", str(ESPTOOL_PATH))
+#     esptool = importlib.util.module_from_spec(esptool_spec)
+#     esptool_spec.loader.exec_module(esptool)
+# 
+#     return esptool
+# 
+# esptool = load_esptool_module()
+#
+# def reset_feather():
+#     print("Resetting feather...")
+#     esp_loader = esptool.ESPLoader.detect_chip(port=FEATHER_DEVICE)    
+#     esp_loader.soft_reset(True)
+#     print("Reset feather.")
 
-    esptool_spec = importlib.util.spec_from_file_location("esptool", str(ESPTOOL_PATH))
-    esptool = importlib.util.module_from_spec(esptool_spec)
-    esptool_spec.loader.exec_module(esptool)
+def read_feather_sensors(queue):
+    FEATHER_ENTER_REPL_NUM_RETRIES = 3
 
-    return esptool
-
-esptool = load_esptool_module()
-
-
-def reset_feather():
-    print("Resetting feather...")
-    esp_loader = esptool.ESPLoader.detect_chip(port=FEATHER_DEVICE)    
-    esp_loader.soft_reset(True)
-    print("Reset feather.")
-
-def main():
-    with open(FEATHER_MAIN_PATH, "r", encoding="utf-8") as f:
-        feather_main_contents = f.read()
-
-    output_chunks = list()
+    _output_chunks = list()
     def on_feather_output(raw):
         chunk = raw.decode("utf-8", errors="replace")
         split_chunks = chunk.split("\n")
 
         if len(split_chunks) > 1:
-            print("".join([*output_chunks, split_chunks[0]]))
+            # TODO : send data back on queue
+            print("".join([*_output_chunks, split_chunks[0]]))
             for line_chunk in split_chunks[1:-1]:
                 print(line_chunk)
-            output_chunks.clear()
-            output_chunks.append(split_chunks[-1])
-        else:
-            output_chunks.append(split_chunks[0])
 
-    # TODO : remove
-    # reset_feather()
-    pyb = pyboard.Pyboard(FEATHER_DEVICE, 115200)
+            _output_chunks.clear()
+            _output_chunks.append(split_chunks[-1])
+        else:
+            _output_chunks.append(split_chunks[0])
+
+    logging.info("Initializing feather...")
+    feather_pyboard = pyboard.Pyboard(FEATHER_DEVICE, FEATHER_BAUD_RATE)
     try:
-        for repl_attempt in range(1, 4):
+        for enter_repl_attempt in range(1, FEATHER_ENTER_REPL_NUM_RETRIES + 1):
             try:
-                print("Entering raw repl attempt: {}".format(repl_attempt))
-                pyb.enter_raw_repl()
+                feather_pyboard.enter_raw_repl()
             except pyboard.PyboardError:
                 pass
             else:
+                if enter_repl_attempt != 1:
+                    logging.warn(f"Entering repl on feather took {enter_repl_attempt} attempts")
                 break
         else:
-            raise RuntimeError("Failed to enter raw repl")
+            raise RuntimeError("Feather failed to enter repl")
 
-        print("Removing and reputting feather libs...")
-        pyb.exec(
+        logging.info("Removing and reputting feather libs...")
+        feather_pyboard.exec(
 """
 import os
 try:
@@ -77,15 +81,34 @@ else:
 
         for lib_path in FEATHER_LIB_DIR_PATH.iterdir():
             assert lib_path.is_file()
-            pyb.fs_put(lib_path, f"/lib/{lib_path.name}")
+            feather_pyboard.fs_put(lib_path, f"/lib/{lib_path.name}")
 
-        print("Exec-ing feather main program...")
-        pyb.exec(feather_main_contents, data_consumer=on_feather_output)
+        with open(FEATHER_MAIN_PATH, "r", encoding="utf-8") as f:
+            feather_main_contents = f.read()
+
+        logging.info("Feather initialization complete.")
+
+        logging.info("Exec-ing feather main program...")
+        feather_pyboard.exec(feather_main_contents, data_consumer=on_feather_output)
     finally:
-        pyb.exit_raw_repl()
-        pyb.close()
+        logging.info("Cleaning up feather.")
+        feather_pyboard.exit_raw_repl()
+        feather_pyboard.close()
 
     print("done")
+
+def main():
+    logging.info("Starting sensor reading gathering processes...")
+    mp.set_start_method("forkserver")
+    sensor_reading_queue = mp.Queue()
+    feather_proc = mp.Process(target=read_feather_sensors, args=(sensor_reading_queue,))
+
+    try:
+        feather_proc.start()
+    finally:
+        feather_proc.join()
+
+    logging.info("Exiting.")
 
 if __name__ == "__main__":
     main()
