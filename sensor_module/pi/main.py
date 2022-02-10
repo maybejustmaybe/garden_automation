be6a8f8110921ef6071901f2308391e8ae92fde7
maybejustmaybe <maybejustmaybe@gmail.com>
2022-02-10 16:10:23 -0500

    test

--- sensor_module/pi/main.py
+++ sensor_module/pi/main.py
@@ -232,6 +232,10 @@ def publish_sensor_readings(sensor_reading_queue):
 
 
 def main():
+    # TODO
+    publish_sensor_readings(mp.Queue())
+    return
+
     SENSOR_PROC_POLL_PERIOD_S = .5
 
     mp.set_start_method("forkserver")
